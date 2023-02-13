34db47ffebaab8985b1ef80efa133cdf2d76b037
Guilherme Neves da Silva <guilhermenevesdasilva321@gmail.com>
2023-02-13 18:43:44 -0300

    Vamos

---
+++ calculadora/js/settings.js
@@ -0,0 +1,15 @@
+{
+    "editor.formatOnSave": true
+}
+var salario-bruto =;
+var dependentes = ;
+var irrf =  ;
+
+switch (true) {
+    case salario-bruto<1903.98:
+        
+        break;
+
+    default:
+        break;
+}
